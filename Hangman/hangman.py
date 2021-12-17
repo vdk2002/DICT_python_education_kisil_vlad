afca423f5697662f6ebf77126dffc6bd34987dc2
vdk2002 <vlad2002ksl@gmail.com>
2021-12-17 11:38:56 +0200

    hangman [5-th] stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,12 +1,25 @@
 import random
 print("HANGMAN")
 print("The game will be available soon.")
-list_words = ['python', 'javascript', 'html', 'java']
-answer_prog = random.choice(list_words)
-help_prog = answer_prog[0] + answer_prog[1] + answer_prog[2] + '-' * (len(answer_prog) - 3)
-print("Guess the word " + help_prog + ":")
-answer_user = input()
-if answer_user == answer_prog:
-    print("You survived!")
-else:
-    print("You lost!")
+words = ['python', 'javascript', 'html', 'java']
+answer_prog = random.choice(words)  #python
+word_list = list(answer_prog)   # [p,y,t,h,o,n]
+user_word_list_null = "-" * len(answer_prog)    # ------
+user_list = list(user_word_list_null)    # [-,-,-,-,-,-]
+print(user_word_list_null)
+count = 0
+while count != 8:
+    count += 1
+    answer_user = str(input('Input a letter:'))
+    if answer_user in word_list:
+        if word_list.count(answer_user) >= 2:
+            index = word_list.index(answer_user)
+            user_list[index] = answer_user
+            word_list[index] = '-'
+        index = word_list.index(answer_user)
+        user_list[index] = answer_user
+    else:
+        print("That letter doesn't appear in the word")
+    print(''.join(user_list))
+print("Thanks for playing!")
+print("We'll see how well you did in the next stage")
