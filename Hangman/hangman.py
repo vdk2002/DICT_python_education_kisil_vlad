ea97bc636c3d92d2795d1ab1e7591421ce518d2d
vdk2002 <vlad2002ksl@gmail.com>
2021-12-17 11:41:34 +0200

    hangman [6-th] stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,16 +1,23 @@
 import random
+
 print("HANGMAN")
 print("The game will be available soon.")
 words = ['python', 'javascript', 'html', 'java']
-answer_prog = random.choice(words)  #python
-word_list = list(answer_prog)   # [p,y,t,h,o,n]
-user_word_list_null = "-" * len(answer_prog)    # ------
-user_list = list(user_word_list_null)    # [-,-,-,-,-,-]
+answer_prog = random.choice(words)  # python
+word_list = list(answer_prog)  # [p,y,t,h,o,n]
+user_word_list_null = "-" * len(answer_prog)  # ------
+user_list = list(user_word_list_null)  # [-,-,-,-,-,-]
+user_used = []
 print(user_word_list_null)
 count = 0
 while count != 8:
     count += 1
     answer_user = str(input('Input a letter:'))
+    if answer_user in user_used:
+        print('No improvements')
+        print(''.join(user_list))
+        continue
+    user_used.append(answer_user)
     if answer_user in word_list:
         if word_list.count(answer_user) >= 2:
             index = word_list.index(answer_user)
@@ -18,8 +25,13 @@ while count != 8:
             word_list[index] = '-'
         index = word_list.index(answer_user)
         user_list[index] = answer_user
-    else:
+        count -= 1
+    elif answer_user not in (user_used and word_list):
         print("That letter doesn't appear in the word")
     print(''.join(user_list))
-print("Thanks for playing!")
-print("We'll see how well you did in the next stage")
+word_list = list(answer_prog)
+if user_list != word_list:
+    print("You lost!")
+else:
+    print("You guessed the word!")
+    print("You survived!")
