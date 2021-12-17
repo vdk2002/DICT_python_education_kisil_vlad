e3118c59bef0948b081aa398c1de28941423a1f4
vdk2002 <vlad2002ksl@gmail.com>
2021-12-17 11:37:13 +0200

    hangman [4-th] stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,9 +1,10 @@
 import random
 print("HANGMAN")
 print("The game will be available soon.")
-list_words = ['python', 'javascript', 'php', 'java']
+list_words = ['python', 'javascript', 'html', 'java']
 answer_prog = random.choice(list_words)
-print("Guess the word:")
+help_prog = answer_prog[0] + answer_prog[1] + answer_prog[2] + '-' * (len(answer_prog) - 3)
+print("Guess the word " + help_prog + ":")
 answer_user = input()
 if answer_user == answer_prog:
     print("You survived!")
