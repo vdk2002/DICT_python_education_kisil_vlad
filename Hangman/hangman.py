f74c80279b9a392d99cfb4ff13016694ecbbb9c5
vdk2002 <vlad2002ksl@gmail.com>
2021-12-17 11:34:09 +0200

    hangman [2-nd] stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,2 +1,8 @@
 print("HANGMAN")
 print("The game will be available soon.")
+print("Guess the word:")
+answer = input()
+if answer == python:
+    print("You survived!")
+else:
+    print("You lost!")
